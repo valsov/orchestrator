2d651e10e31aa9bdb7e02c1b2ca8f11348f8c5de
valsov <valentin.sovignet@sfr.fr>
2023-12-07 22:48:11 +0100

    Sample code to start and stop a container

--- main.go
+++ main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"fmt"
+	"orchestrator/task"
+	"orchestrator/worker"
+	"time"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+func main() {
+	db := make(map[uuid.UUID]*task.Task)
+	w := worker.Worker{
+		Db:    db,
+		Queue: *queue.New(),
+	}
+
+	t := task.Task{
+		Id:    uuid.New(),
+		Name:  "test-container",
+		State: task.Scheduled,
+		Image: "strm/helloworld-http",
+	}
+
+	fmt.Println("starting task")
+	w.AddTask(&t)
+	result := w.RunTask()
+	if result.Error != nil {
+		panic(result.Error)
+	}
+
+	fmt.Printf("task %v is running in container %s\n", t.Id, result.ContainerId)
+	time.Sleep(time.Second * 10)
+
+	fmt.Print("stopping task")
+	t.State = task.Completed
+	w.AddTask(&t)
+	result = w.RunTask()
+	if result.Error != nil {
+		panic(result.Error)
+	}
+}
