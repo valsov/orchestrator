8ff9cd9722880a06f8ee5f255540ea326fdc16b5
valsov <valentin.sovignet@sfr.fr>
2023-12-12 07:50:20 +0100

    Round robin scheduler

---
+++ scheduler/round_robin.go
@@ -0,0 +1,54 @@
+package scheduler
+
+import (
+	"orchestrator/node"
+	"orchestrator/task"
+)
+
+type RoundRobin struct {
+	Name       string
+	LastWorker int
+}
+
+func (r *RoundRobin) SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node {
+	return nodes
+}
+
+func (r *RoundRobin) Score(t task.Task, nodes []*node.Node) map[string]float64 {
+	if len(nodes) == 0 {
+		return nil
+	}
+	var newWorker int
+	if r.LastWorker == len(nodes)-1 {
+		newWorker = 0
+	} else {
+		newWorker = r.LastWorker + 1
+	}
+	r.LastWorker = newWorker
+
+	scores := make(map[string]float64, len(nodes))
+	for i, node := range nodes {
+		if i == newWorker {
+			scores[node.Name] = 0.1
+		} else {
+			scores[node.Name] = 1
+		}
+	}
+	return scores
+}
+
+func (r *RoundRobin) Pick(scores map[string]float64, nodes []*node.Node) *node.Node {
+	if len(nodes) == 0 {
+		return nil
+	}
+
+	lowest := scores[nodes[0].Name]
+	selected := nodes[0]
+	for _, node := range nodes {
+		if scores[node.Name] < lowest {
+			lowest = scores[node.Name]
+			selected = node
+		}
+	}
+	return selected
+}
--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,12 @@
 package scheduler
 
+import (
+	"orchestrator/node"
+	"orchestrator/task"
+)
+
 type Scheduler interface {
-	SelectCandidateNodes()
-	Score()
-	Pick()
+	SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node
+	Score(t task.Task, nodes []*node.Node) map[string]float64
+	Pick(scores map[string]float64, nodes []*node.Node) *node.Node
 }
